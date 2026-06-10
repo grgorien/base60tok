bd6b29515bc3d82b46cbdb54425a631f833ce500
William Gregorio <william.gregorio.ny@gmail.com>
2026-06-10 18:10:23 -0400

    app init Gtk

--- main.c
+++ main.c
@@ -8,3 +8,22 @@ typedef struct {
   gint64 elapsed_time;
   gboolean is_running;
 } Base60;
+int main(int argc, char **argv) {
+  GtkApplication *app;
+  int status;
+
+  Base60 app_data = {.label = NULL,
+                     .timer_id = 0,
+                     .start_time = 0,
+                     .elapsed_time = 0,
+                     .is_running = FALSE};
+
+  app = gtk_application_new("com.base60tok.stopwatch",
+                            G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(on_activate), &app_data);
+
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
+}
